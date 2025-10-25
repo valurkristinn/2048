58cd5e84a39c65ab97d565d3bcf950c899d3003e
Valurkristinn <109678364+Valurks@users.noreply.github.com>
2025-10-25 12:55:32 +0000

    added move validation function

--- gameLogic.c
+++ gameLogic.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 int board[16];
+enum Direction { UP = -4, DOWN = 4, LEFT = -1, RIGHT = 1 };
 
 int *getEmptyTiles(int *count) {
     int *emptyTiles = calloc(16, sizeof(int));
@@ -25,11 +26,20 @@ void newTile() {
     board[tileNum] = tileVal;
 }
 
-void isMoveValid(int tilenum, int direction) {}
+int isMoveValid(int tilenum, enum Direction direction) {
+    int currentTileVal  = board[tilenum];
+    int nextTileVal = board[tilenum+direction];
+    return currentTileVal == nextTileVal || nextTileVal == 0;
+}
 
-void move(int direction) {}
+void move(enum Direction direction) {
+
+}
 
 void display() {
+    for (int i = 0; i < 99; i++) {
+        printf("\n");
+    }
     for (int i = 0; i < 16; i++) {
         printf("%d ", board[i]);
         if (i % 4 == 3) {
